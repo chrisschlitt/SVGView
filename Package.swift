efe79b37c8e588ffa122d49a2134cf9a0bb71901
Chris Schlitt <chs5056@icloud.com>
2022-08-18 14:48:55 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,8 +7,7 @@ let package = Package(
 	platforms: [
 		.macOS(.v11),
         .iOS(.v14),
-        .watchOS(.v7),
-        .tvOS(.v13)
+        .watchOS(.v7)
     ],
     products: [
     	.library(
